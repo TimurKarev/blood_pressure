ced977cf793665ecb9f32bb95b2ac454d0045936
timurkarev <timurkarev@mail.ru>
2021-10-01 11:29:56 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:blood_pressure/pages/landing_page.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: 'Flutter Demo',
+      home: LandingPage(),
+    );
+  }
+}
